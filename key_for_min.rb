1c23bf6c2059bdfbce8bff7643b5ff95ac723a68
Matt Im <matthew.seiim@gmail.com>
2020-05-03 18:35:54 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,7 +1,9 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(test_score)
-  test_score = {:reading => 90, :math => 95, :spelling => 99}
+def key_for_min_value(test_scores)
+  test_scores = {:reading => 90, :math => 95, :spelling => 99}
+  test_scores.each do |subject, value|
+    if subject < 90 
 key_for_min_value(test_score)
 end
