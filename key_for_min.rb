da981d4cd4b2d7a31da52d07f8bae2cb5e0857ab
Matt Im <matthew.seiim@gmail.com>
2020-05-03 18:26:01 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,6 +2,6 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(test_score)
-  test_score = {:reading => 90. :math => 95, :spelling => 99}
+  test_score = {:reading => 90, :math => 95, :spelling => 99}
 key_for_min_value(reading)
 end
