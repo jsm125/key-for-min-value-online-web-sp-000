ab0fba4438d54602aa66fecc1e53b2b30d49178b
Matt Im <matthew.seiim@gmail.com>
2020-05-03 19:01:39 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,14 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(test_scores)
+  lowest_value = 0
+  lowest_key = nil
+  
   test_scores.each do |subject, value|
-    if subject < 90 
-key_for_min_value(test_score)
+    if lowest_value == 0 || value < lowest_value
+      lowest_value = value
+      lowest_key = subject
+    end
+  end
+  lowest_key
 end
